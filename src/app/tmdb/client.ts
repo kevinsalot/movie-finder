aa9efeb1a1a4a48da09486f04ef38091801c675b
Kevin Ellis <kevin.m.ellis@gmail.com>
2024-02-24 19:16:51 -0600

    chore: revoked key, pulled from repo

--- src/app/tmdb/client.ts
+++ src/app/tmdb/client.ts
@@ -1,12 +1,11 @@
 import createClient, { Middleware } from "openapi-fetch";
 import { paths } from "../../../tmdb-api";
+import { tmdbAccessToken } from "../../../private";
 
 const apiUrl = "https://api.themoviedb.org";
-const accessToken: string =
-  "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzZTE4MDcxMDQyYjQ0Y2I2NTA2NTdhMGJhNjZiYzc4NSIsInN1YiI6IjVhZTcxOGNmOTI1MTQxMGNkNTAwOGNmNiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.MDtIoQp5ZY8nz9L6oo_7lO3TsoLxZ1G6E_pcVVbX3jc";
 const authMiddleware: Middleware = {
   async onRequest(req) {
-    req.headers.set("Authorization", `Bearer ${accessToken}`);
+    req.headers.set("Authorization", `Bearer ${tmdbAccessToken}`);
     return req;
   },
 };
